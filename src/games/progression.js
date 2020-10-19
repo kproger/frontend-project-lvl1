304704e311491ca90a820cb7997472703ecaa534
Bekaev Sharif <bekaevsharif@mail.ru>
2020-10-19 14:38:55 +0300

    Progression game. Global value added

--- src/games/progression.js
+++ src/games/progression.js
@@ -2,25 +2,26 @@ import gameArc from '../index.js';
 import { getRandomNumber } from '../utils.js';
 
 const gameConditions = 'What number is missing in the progression?';
+const progressionLength = 10;
 
-const genProgression = (firstElem, indexToDelete, step) => {
+const genProgression = (firstElem, indexToHide, step, progresLen) => {
   const result = [];
 
-  for (let i = 0; i < 10; i += 1) {
+  for (let i = 0; i < progresLen; i += 1) {
     const currentElem = firstElem + step * i;
-    const elem = i === indexToDelete ? '..' : currentElem;
+    const elem = i === indexToHide ? '..' : currentElem;
     result.push(elem);
   }
 
-  return result.join(' ');
+  return result;
 }
 
 const getQustnAndCorAnswer = () => {
   const firstElem = getRandomNumber(1, 100);
   const stepAmount = getRandomNumber(1, 10);
-  const indexToDelete = getRandomNumber(0, 9);
-  const question = genProgression(firstElem, indexToDelete, stepAmount);
-  const correctAnswer = (firstElem + stepAmount * indexToDelete).toString();
+  const hiddenElemIndex = getRandomNumber(0, 9);
+  const question = genProgression(firstElem, hiddenElemIndex, stepAmount, progressionLength).join('');
+  const correctAnswer = (firstElem + stepAmount * hiddenElemIndex).toString();
 
   return [question, correctAnswer];
 }
